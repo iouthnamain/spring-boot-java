760ccf8cb35b804a134a0427daaf0370ef379c61
iouthnamain <iouthnamain@gmail.com>
2024-05-19 00:36:24 +0700

    end of tut on spring boot

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -23,5 +21,23 @@ public class StudentController {
     @GetMapping
     public List<Student> getAllStudents(){
         return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public void registerStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentID}")
+    public void deleteStudent(@PathVariable("studentID") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentID}")
+    public void updateStudent(
+            @PathVariable("studentID") Long studentID,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentID, name, email);
     }
 }
